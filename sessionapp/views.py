382eb0fd1808d3ca07ff1adb2d4a51af1817d302
lokeshrapala <lokesh.rapala>
2018-08-09 12:26:26 +0530

    Initial commit

---
+++ sessionapp/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def input(request):
+    return render(request,'base.html')
+def add(request):
+   x=int(request.GET['t1'])
+   y=int(request.GET['t2'])
+   z=x+y
+   request.session['z'] =z
+   request.session.set_expiry(30)
+   return HttpResponse('data submitted successfully')
+def display(request):
+    if request.session.has_key('z'):
+        z = request.session['z']
+        return HttpResponse(z)
+    else:
+        return render(request,'base.html')
+
+
+
